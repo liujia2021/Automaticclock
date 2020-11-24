6ade68edd80f424706f5471ce94186051ae2f96c
liujia2021 <74963175+liujia2021@users.noreply.github.com>
2020-11-24 19:33:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -139,6 +139,7 @@ def wechatPush(title,sckey,success,fail,result):
     strTime = getNowTime()
     page = json.dumps(result.json(), sort_keys=True, indent=4, separators=(',', ': '),ensure_ascii=False)
     content = f"""
+ #### 打卡时间：
 `{strTime}` 
 #### 打卡成功用户：
 `{success}` 
